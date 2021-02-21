02d1c01282fdb69eb8adca6b2790613ec1cb640b
maulanarifai114 <maulanarifai114@gmail.com>
2021-02-21 13:45:31 +0700

    change get all labels

--- src/controllers/labels/getAllLabels.js
+++ src/controllers/labels/getAllLabels.js
@@ -2,15 +2,11 @@ const { response, reject } = require('../../helpers/helper')
 const { getAllLabels } = require('../../models/labels')
 
 exports.getAllLabels = (req, res) => {
-  if (req.id.toString() === process.env.ADMIN) {
-    getAllLabels()
-      .then((result) =>
-        result.length === 0
-          ? response(res, 'Labels is empty', 200, null)
-          : response(res, result, 200, null)
-      )
-      .catch((err) => reject(res, null, 500, { msg: 'Database is error', err }))
-  } else {
-    reject(res, null, 401, 'You are not an Administrator')
-  }
+  getAllLabels()
+    .then((result) =>
+      result.length === 0
+        ? response(res, 'Labels is empty', 200, null)
+        : response(res, result, 200, null)
+    )
+    .catch((err) => reject(res, null, 500, { msg: 'Database is error', err }))
 }
